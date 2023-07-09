0bacecc0875d5476f21246351acb0b0132c4b124
Bill-Ukraine <chermetbeloff@gmail.com>
2023-07-09 15:38:46 +0300

    ButtonTheme

---
+++ src/js/header/color-scheme-switcher.js
@@ -0,0 +1,25 @@
+const themeSwitcherEl = document.querySelector('.switcher-theme');
+themeSwitcherEl.checked = false;
+function onHandlerClick() {
+  if (themeSwitcherEl.checked) {
+    document.body.classList.remove('light');
+    document.body.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.body.classList.add('light');
+    document.body.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+  }
+}
+themeSwitcherEl.addEventListener('click', onHandlerClick);
+
+window.onload = checkTheme();
+
+function checkTheme() {
+  const localStorageTheme = localStorage.getItem('theme');
+
+  if (localStorageTheme !== null && localStorageTheme === 'dark') {
+    document.body.classList = localStorageTheme;
+    themeSwitcherEl.checked = true;
+  }
+}
