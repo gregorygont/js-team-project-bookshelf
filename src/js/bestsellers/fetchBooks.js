6dfb4ebc3e37f2cc75c7db142fd607586d36d787
Roman Telik <rtelik58@gmail.com>
2023-07-09 15:16:21 +0300

    add/best

---
+++ src/js/bestsellers/fetchBooks.js
@@ -0,0 +1,19 @@
+import Notiflix from 'notiflix';
+
+export default async function getTopBooksArray() {
+  Notiflix.Loading.dots();
+  // Notiflix.Loading.pulse();
+
+  try {
+    const { data } = await axios.get(
+      'https://books-backend.p.goit.global/books/top-books'
+    );
+    Notiflix.Loading.remove();
+
+    return data;
+  } catch (error) {
+    Notiflix.Loading.remove();
+    Notiflix.Notify.failure('Something went wrong. Please try again');
+    console.log(error);
+  }
+}
