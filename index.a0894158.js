7b916879d28e50fc6fe4b694956d81f449bed1a5
gregorygont <105290753+gregorygont@users.noreply.github.com>
2023-07-09 18:04:03 +0000

    Deploying to gh-pages from @ gregorygont/js-team-project-bookshelf@7bd57b90c9fd245fca3b5ff0bb8b40fb57f9981e 🚀

---
+++ index.a0894158.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".switcher-theme");e.checked=!1,console.log(e),e.addEventListener("click",function(){e.checked?(document.body.classList.remove("light"),document.body.classList.add("dark"),localStorage.setItem("theme","dark")):(document.body.classList.add("light"),document.body.classList.remove("dark"),localStorage.setItem("theme","light"))}),window.onload=function(){let t=localStorage.getItem("theme");null!==t&&"dark"===t&&(document.body.classList=t,e.checked=!0)}();
+//# sourceMappingURL=index.a0894158.js.map
