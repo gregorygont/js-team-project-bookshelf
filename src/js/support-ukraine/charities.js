8a0d14a74f97a432eeaac93a29c5a9a6f101cfa4
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-07-09 10:27:26 +0300

    Support Ukraine
    без підключення бібліотеки для слайдера та без картинок

---
+++ src/js/support-ukraine/charities.js
@@ -0,0 +1,57 @@
+export const galleryItems =[
+{
+title: 'Save the Children',
+url:
+'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis ',
+        img: null,
+number: '1',
+},
+{
+title: 'Project HOPE',
+url: 'https://www.projecthope.org/country/ukraine/ ',
+    img: null,
+number: '2',
+},
+{
+title: 'UNITED24',
+url: 'https://u24.gov.ua/uk',
+    img: null,
+number: '3',
+},
+{
+title: 'International Medical Corps',
+url: 'https://internationalmedicalcorps.org/country/ukraine/ ',
+    img: null,
+number: '4',
+},
+{
+title: 'Medicins Sans Frontieres',
+url: 'https://www.msf.org/ukraine',
+    img: null,
+number: '5',
+},
+{
+title: 'RAZOM',
+url: 'https://www.razomforukraine.org/ ',
+    img: null,
+number: '6',
+},
+{
+title: 'Action against hunger',
+url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: null,
+number: '7',
+},
+{
+title: 'World vision',
+url: 'https://www.wvi.org/emergencies/ukraine',
+    img: null,
+number: '8',
+},
+{
+title: 'Serhiy Prytula Charity Foundation',
+url: 'https://prytulafoundation.org/en',
+    img: null,
+number: '9',
+},
+]
---
+++ src/js/support-ukraine/support.js
@@ -0,0 +1,17 @@
+import { galleryItems } from "./charitableFoundation";
+
+const galleryEl = document.querySelector('.support-list')
+console.log(galleryEl);
+const card = createPictureGalery(galleryItems)
+galleryEl.insertAdjacentHTML('beforeend', card)
+function createPictureGalery (item){
+return galleryItems.map(({title, url, img,number}) =>{
+    return`
+    <li class="gallery__item"><span class="number__item">${number}</span>
+   <a class="gallery__link" href="${url}">
+      <img class="gallery__image" src="${img}" alt="${title}" />
+   </a>
+</li>
+    `  
+}).join("");
+}
