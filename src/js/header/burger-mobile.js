c91103086f5543ae4b19b92052509f61fd44969d
MaksymHolovchuk <122921772+MaksymHolovchuk@users.noreply.github.com>
2023-07-11 08:54:24 +0300

    Update burger-mobile.js

---
+++ src/js/header/burger-mobile.js
@@ -0,0 +1,31 @@
+const refs = {
+  openMenuBtn: document.querySelector('[data-menu-open]'),
+  menu_open: document.querySelector('.mobile-menu-open__icon'),
+  menu_close: document.querySelector('.mobile-menu__close-icon'),
+  menu: document.querySelector('[data-menu]'),
+};
+refs.openMenuBtn.addEventListener('click', toggleModal);
+function toggleModal() {
+  refs.menu.classList.toggle('is-open');
+  refs.menu_close.classList.toggle('is-hidden');
+  refs.menu_open.classList.toggle('is-hidden');
+  if (refs.menu_open.classList.contains('is-hidden')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+}
+function setActiveClass() {
+  const burgerNavLinks = document.querySelectorAll('.burger-nav-link');
+  burgerNavLinks.forEach(link => {
+    if (
+      link.href === window.location.href ||
+      link.href === window.location.href + 'index.html'
+    ) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+setActiveClass();
